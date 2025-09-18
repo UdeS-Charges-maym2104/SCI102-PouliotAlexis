267251cf0470d3ce052339a7002530c040112a66
PouliotAlexis <alex@harpou.com>
2025-09-18 07:29:46 -0400

    Implémentation de la première fonctionnalité

--- labo2.py
+++ labo2.py
@@ -1,5 +1,5 @@
 def salutations(name):
     return "Bonjour" + name
 
-def age(birth_year):
-    return "Votre âge est " + (2025 - birth_year)
+# def age(birth_year):
+#     return "Votre âge est " + (2025 - birth_year)
